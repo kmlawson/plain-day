253e5976c3aec862c5d38fe80cd1c0c71827dc6a
K. M. Lawson <raven@muninn.net>
2013-04-16 16:45:41 +0200

    updated gem for more yesterday options and cleaned up a bit

--- lib/pday.rb
+++ lib/pday.rb
@@ -16,17 +16,39 @@ def isdate testdate
     end
 end
 
+# REVISIT THIS TO MAKE THE HEADER TEXT CUSTOMIZABLE
 # Default to a Header 1 in Markdown:
 def prepopulate headertext
     return "# "+headertext
 end
 
+def formatdate requesteddate, dateformat
+    if requesteddate=='yesterday' || requesteddate=='y'
+        formateddate=(Time.now-86400)
+    elsif requesteddate=='yy' # Day before yesterday
+        formateddate=(Time.now-172800)
+    elsif requesteddate=='tomorrow' || requesteddate=='t'
+        formateddate=(Time.now+86400)
+    elsif requesteddate=='yyy' # Three days ago
+        formateddate=(Time.now-259200)
+    else
+        if isdate(requesteddate)
+            formateddate=Date.parse(requesteddate)
+        else
+            return nil
+        end
+    end
+    return formateddate.strftime(dateformat)
+end
 
 main do
+
+    #Set up default options
     default_options=Hash.new
     default_options['suffix-delimit'] = '-'
     default_options['format'] = 'md'
     default_options['editor'] = 'vim'
+    default_options['date-format'] = "%Y.%m.%d"
     # Load the .dayconfig file and merge in options there:
     CONFIG_FILE = File.join(ENV['HOME'],'.dayconfig') 
     if File.exists? CONFIG_FILE
@@ -38,50 +60,61 @@ main do
     end
     default_options.merge!(options)
     options.merge!(default_options)
+
+
     # Check if the diary directory is found:
     unless options['path']
         puts "Could not find path to your diary. Did you configure the .dayconfig file?"
         exit
     end
+    
     # Use today's date unless there is one supplied
     unless options[:date]
-        entrydate=Time.now.strftime("%Y.%m.%d")
+        entrydate=Time.now.strftime(options['date-format'])
     else
-        entrydate=options['date']
+        # Check date
+        finaldate=formatdate(options['date'],options['date-format'])
+        unless finaldate!=nil
+            puts "DATE must be a valid date. For example '2013.1.15' or '01/15/2013' or 'Jan 1, 2013' or 'yesterday'"
+            exit
+        else
+            entrydate=finaldate
+        end
     end
+
     # Check to see if there is a suffix, and if so, add the suffix delimiter:
     if options[:suffix]
         suffix=options['suffix-delimit']+options['suffix']
     else
         suffix=''
     end
-    # Build the command:
+
+
+    # Build the command to create the file with the header and edit it:
     filepath=options['path']+entrydate+suffix+'.'+options['format']
-    puts "Creating new entry for: "+entrydate
-    `echo '#{prepopulate(entrydate)}' > '#{filepath}'`
-    puts "Editing entry..."
-    `#{options['editor']} '#{filepath}'`
+    if File.file?(filepath)
+        unless options[:force]
+            puts "File already exists. Will open existing file."
+        else
+            puts "File already exists, replacing it."
+            `echo '#{prepopulate(entrydate)}' > "#{filepath}"`
+        end
+    else
+        puts "Creating new entry for: "+entrydate
+        `echo '#{prepopulate(entrydate)}' > "#{filepath}"` # NEED TO ESCAPE FILEPATH FOR SPACES
+    end
+    puts "Editing entry: #{filepath}"
+    `#{options['editor']} "#{filepath}"`
 end
 
-version     '0.2'
+version     '0.3'
 description 'Simple plain text diary management script.'
 
-opts.on("-d DATE","--date","Supply an explicit date. Also accepts 'yesterday' or 'y'") do |date|
-    if date=='yesterday' || date=='y'
-        date=(Time.now-86400).strftime("%Y.%m.%d")
-    end
-
-    unless isdate(date)
-        puts "DATE must be a valid date. For example \
-        '2013.1.15' or '01/15/2013' or 'Jan 1, 2013' or \
-        'yesterday'"
-    end
-    options[:date]=true
-    options['date']=Date.parse(date).strftime("%Y.%m.%d")
-end
-
-on("-f FORMAT","--format","Set format suffix")
-on("-s SUFFIX","--suffix","Add suffix")
+on("-t FORMAT","--format","Set format suffix")
+on("-f","--force","If a file already exists with that name, replace it instead of editing existing")
+on("-s SUFFIX","--suffix","Add suffix to file name (before format suffix)")
 on("-e EDITOR","--editor","Set the editor, vim, emacs, nano, etc.")
+on("-d DATE","--date","Supply an explicit date. Also accepts 'yesterday' or 'y'") 
+
 
 go!
--- pday.gemspec
+++ pday.gemspec
@@ -1,12 +1,12 @@
 Gem::Specification.new do |s|
     s.name  = 'pday'
-    s.version = '0.2.0'
+    s.version = '0.3.0'
     s.summary = "Simple plain text diary management script."
     s.description = "Simple shortcut script for creating a new plain text diary entry with today's date."
     s.authors = "Konrad M. Lawson"
     s.email = "regs@huginn.net"
     s.files = ["lib/pday.rb"]
-    s.date = "2013-04-13"
+    s.date = "2013-04-16"
     s.homepage = 'http://github.com/kmlawson/plain-day/'
     s.license = 'MIT'
     s.executables << 'pday'
